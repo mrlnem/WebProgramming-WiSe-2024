6a6391d6955b7da8b3f34ce5bc4fbcb0ce74109e
mrlnem <muchm@dhbw-loerrach.de>
2024-10-15 15:48:47 +0200

    tried to make a function

--- 15.10.24/my_scrpit.js
+++ 15.10.24/my_scrpit.js
@@ -1,5 +1,8 @@
-while(fibo.length < 10){
+function(fibo) {
+
+    while(fibo.length < 10){
     fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2]);
 }
 
 console.log(fibo); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+}
